4f715f5488736845553e85d822b90ded9087b7a3
Joakim <jokenordstrom@hotmail.com>
2018-05-29 10:51:06 +0200

    Fixed playerPrefs

--- Assets/Scripts/UI/Menu Scripts/MainMenu.cs
+++ Assets/Scripts/UI/Menu Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour {
     //Load the first level in the game.
     public void NewGame()
     {
+        ResetAllPlayerPrefs();
         SceneManager.LoadScene("Level 1");
         Debug.LogWarning("Change scene in NewGame method to correct scene please!");
     }
@@ -41,5 +42,13 @@ public class MainMenu : MonoBehaviour {
     {
         Debug.Log("Load last non-completed scene.");
         SceneManager.LoadScene(PlayerPrefs.GetString("LastLevelPlayed"));
+    }
+
+    public void ResetAllPlayerPrefs()
+    {
+        PlayerPrefs.SetFloat("TotalTime", 0);
+        PlayerPrefs.SetFloat("TotalDeathCount", 0);
+        PlayerPrefs.SetFloat("TotalBloodLoss", 0);
+        PlayerPrefs.SetFloat("TotalScore", 0);
     }
 }
