f8a0ed3f804671910d444ee7b0bab78c163df45d
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2018-05-31 14:16:36 +0200

    added splashscreen

---
+++ Assets/SplashScreenScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenScript : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+        yield return new WaitForSeconds(32f);
+        SceneManager.LoadScene("MainMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+	}
+}
